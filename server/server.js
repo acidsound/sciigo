c8678cb9de688abb4465685b639fdf36fd679807
acidsound <jhlee@appsoulute.com>
2012-09-26 02:51:36 +0900

    account 설정 내부로 변경

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ Meteor.startup(function () {
   // code to run on server at startup
   console.log("server initiated.");
   console.log("have fun with meteor");
+  Meteor.accounts.configuration.remove({});
+  Meteor.accounts.configuration.insert({ "service" : "facebook", "appId" : "381547438582462", "secret" : "359f864a61d4c9223018df9e66b6a7bc"});
   Messages.remove({});
   Messages.insert({userName:"Lee Jaeho", message:"Hi hello"});
   Messages.insert({userName:"ppillip", message:"Hwhat da hell"});
