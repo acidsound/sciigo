6c81682ec3ab6491f18cd3b15fd5b1fe2fae1ec4
acidsound <jhlee@appsoulute.com>
2012-09-26 21:46:02 +0900

    인증사용자별 글쓰기 기능 추가 시간별 역순 정렬 기능 구현

--- client/client.js
+++ client/client.js
@@ -1,25 +1,36 @@
 Template.hello.greeting = function () {
-  return "세미나에 어서오세요. 제발  "+ Session.get('count')+"번만요";
+  return "세미나에 어서오세요. 제발  " + Session.get('count') + "번만요";
 };
-Template.hello.uptime = function() {
+Template.hello.uptime = function () {
   return moment().format("YYYY/MM/DD hh:mm:ss");
 };
+Template.hello.messages = function () {
+  return Messages.find({},{sort:{createTime:-1}}).map(function (message) {
+    if (Meteor.user()) {
+      message.userType = message.userName === Meteor.user().profile.name ? "success" : "";
+      message.timeAgo=moment(message.createTime).fromNow();
+    }
+    return message;
+  });
+}
 
-Template.hello.events = {
-  'click input' : function () {
-    // template data, if any, is available in 'this'
-    if (typeof console !== 'undefined')
-      console.log("You pressed the button");
-      Session.set("count", Session.get('count')+1)
+Template.hello.loginUser = function () {
+  return Meteor.user() && Meteor.user().profile ? Meteor.user().profile.name : "";
+}
+
+Template.formMessage.userName = function () {
+  return Meteor.user() && Meteor.user().profile ? Meteor.user().profile.name : "";
+}
+
+Template.formMessage.events = {
+  "submit":function () {
+    console.log('submitted');
+    Messages.insert({
+      userName:Meteor.user().profile.name,
+      message:$("input#inputMessage").val(),
+      createTime: Date.now()
+    });
+    $("input#inputMessage").val('').focus();
   }
-};
-
-Template.hello.messages = function() {
-	return Messages.find().map(function(user) {
-		if (Meteor.user()) {
-			user.userType = user.userName === Meteor.user().profile.name ? "success":"";
-		}
-		return user;
-	});
 }
 Session.set('count', 0);
--- server/server.js
+++ server/server.js
@@ -2,10 +2,7 @@ Meteor.startup(function () {
   // code to run on server at startup
   console.log("server initiated.");
   console.log("have fun with meteor");
-  Meteor.accounts.configuration.remove({});
-  Meteor.accounts.configuration.insert({ "service" : "facebook", "appId" : "381547438582462", "secret" : "359f864a61d4c9223018df9e66b6a7bc"});
-  Messages.remove({});
-  Messages.insert({userName:"Lee Jaeho", message:"Hi hello"});
-  Messages.insert({userName:"ppillip", message:"Hwhat da hell"});
-  Messages.insert({userName:"dehol", message:"miku banzai"});
+  if (!Meteor.accounts.configuration.find().count()) {
+    Meteor.accounts.configuration.insert({ "service" : "facebook", "appId" : "381547438582462", "secret" : "359f864a61d4c9223018df9e66b6a7bc"});
+  }
 });
