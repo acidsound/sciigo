9967785a02b9a7ae3b59ebd5050de30282710b33
acidsound <jhlee@appsoulute.com>
2012-09-26 23:34:45 +0900

    TEST/PROD 를 환경변수 ROOT_URL 기준으로 구분.

--- server/server.js
+++ server/server.js
@@ -2,7 +2,12 @@ Meteor.startup(function () {
   // code to run on server at startup
   console.log("server initiated.");
   console.log("have fun with meteor");
+  console.log("%s Mode is running.", __meteor_runtime_config__.ROOT_URL);
   if (!Meteor.accounts.configuration.find().count()) {
-    Meteor.accounts.configuration.insert({ "service" : "facebook", "appId" : "381547438582462", "secret" : "359f864a61d4c9223018df9e66b6a7bc"});
+    Meteor.accounts.configuration.insert(
+      __meteor_runtime_config__.ROOT_URL==="http://localhost:3000" ?
+        { "service" : "facebook", "appId" : "381547438582462", "secret" : "359f864a61d4c9223018df9e66b6a7bc"} :
+        { "service" : "facebook", "appId" : "246175668838235", "secret" : "98159493c46e3975db8c1b764cd96e91"}
+    );
   }
 });
