0b79a8dbc6771cfb0e9c432b6d3a3b38c8a72c11
acidsound <jhlee@appsoulute.com>
2012-09-28 02:01:11 +0900

    서비스 설정을 일반화. test와 prod로 구분하여 config.js에 넣음.

--- server/server.js
+++ server/server.js
@@ -12,10 +12,10 @@ Meteor.startup(function () {
   });
 
   if (!Meteor.accounts.configuration.find().count()) {
-    Meteor.accounts.configuration.insert(
-      __meteor_runtime_config__.ROOT_URL==='http://localhost:3000' ?
-        { "service" : "facebook", "appId" : "381547438582462", "secret" : "359f864a61d4c9223018df9e66b6a7bc"} :
-        { "service" : "facebook", "appId" : "246175668838235", "secret" : "98159493c46e3975db8c1b764cd96e91"}
-    );
+    config[__meteor_runtime_config__.ROOT_URL==='http://localhost:3000'
+      ? "test" : "prod"].services.forEach(function(service) {
+        console.dir(service);
+        Meteor.accounts.configuration.insert(service);
+    });
   }
 });
