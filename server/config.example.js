083519759b9602761a4e6b03b9b9748cdbf003ad
acidsound <jhlee@appsoulute.com>
2012-10-05 15:02:29 +0900

    config.js 는 테스트 환경마다 다를 수 있으므로 config.example.js 로 분리하고 config.js 는 commit 되지 않도록 ignore 함

---
+++ server/config.example.js
@@ -0,0 +1,12 @@
+var config = {
+  test:{
+    services:[
+      { "service":"facebook", "appId":"381547438582462", "secret":"359f864a61d4c9223018df9e66b6a7bc"}
+    ]
+  },
+  prod:{
+    services:[
+      { "service":"facebook", "appId":"246175668838235", "secret":"98159493c46e3975db8c1b764cd96e91"}
+    ]
+  }
+};
