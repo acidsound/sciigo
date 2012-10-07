bcdb5c6e9e55be42c3e03b76dfe07142d00660dc
gaethug <cbak@naver.com>
2012-10-07 11:54:21 +0900

    부 한번 하고 다시 부 누르면 기존 부 취소되게 하였음. msg.myBoo.some안에 index++을 넣었기 때문에 대장님이 싫어하실수도 있음.

--- server/server.js
+++ server/server.js
@@ -41,13 +41,16 @@ Meteor.startup(function () {
       var msg = Messages.findOne({_id:arg.id});
       var user = arg.user;
       msg.myBoo = msg.myBoo || [];
+      var index=-1;
       if (!msg.myBoo.some(function (s) {
-        return user._id === s._id
+          index++;
+          return user._id === s._id
       })) {
         msg.myBoo.push(user);
-        return Messages.update({_id:msg._id}, msg);
+      }else{
+          msg.myBoo.splice(index, 1);
       }
-      return arg.msg;
+      return Messages.update({_id:msg._id}, msg);
     }
   });
 
